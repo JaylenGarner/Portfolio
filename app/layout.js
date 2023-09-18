7704fbd9a13355975ee19011abe8d780ce191112
Jaylen Garner <jaylen@moonraydevelopment.com>
2023-09-17 21:13:51 -0400

    Initialized Vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata = {
   title: "Jaylen Garner",
   description: "Jaylen Garner's Portfolio",
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
