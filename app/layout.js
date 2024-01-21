23408174f45b0239f253a07472012d3ede096d07
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-01-21 13:32:31 -0500

    Connected Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         {children}
         <Analytics />
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
     </html>
   );
 }
